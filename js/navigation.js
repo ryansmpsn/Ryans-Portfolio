621017154d7feeaa7ad94618a24c7d3d9510c0ff
Ryan Simpson <ryan.simpson@postalfleetsvs.com>
2020-11-23 23:20:50 -0500

    Bulb Source

--- js/navigation.js
+++ js/navigation.js
@@ -11,17 +11,16 @@ const navSlide = () => {
       burger.classList.toggle("toggle");
       // Animate Links
       navLinks.forEach((link, index) => {
-        if (link.style.animation) {
-          link.style.animation = "";
-        } else {
-          link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+        if (window.innerWidth < 767) {
+          if (link.style.animation) {
+            link.style.animation = "";
+          } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+          }
         }
       });
     });
   });
 };
 
-function runNavSlide {
-  window.innerWidth < 768 && navSlide();
-
-}
+navSlide();
